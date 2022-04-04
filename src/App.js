1728758217ea8bb5708b38a65030c4b9d794d55b
StojcevskiD <dimitar.stojcevski@hotmail.com>
2022-04-04 13:09:13 +0200

    lab1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import {Routes, Navigate} from "react-router-dom";
+import Book from "./components/Book/Book";
+import Header from "./components/Header/Header";
+import EditBook from "./components/EditBook/EditBook";
+import AddBook from "./components/AddBook/AddBook";
+import Categories from "./components/Categories/Categories";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Header/>
+      <Routes>
+          <Route path={'/books'} element={<Book/>}/>
+          <Route path={'/book/edit/:id'} element={<EditBook/>}/>
+          <Route path={'/book/add'} element={<AddBook/>}/>
+          <Route path={'/categories'} element={<Categories/>}/>
+
+          <Route path="*" element={<Navigate to="/books"/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
